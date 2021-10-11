6f12bb73b098b8ecaf726bacf094f3e7d629da75
macle86 <ysys86a@gmail.com>
2021-10-12 01:12:22 +0900

    0.0.1

--- exchange-modules/binance-java-api/src/main/java/com/binance/api/client/domain/account/request/CancelOrderListResponse.java
+++
@@ -1,9 +0,0 @@
-package com.binance.api.client.domain.account.request;
-
-import com.binance.api.client.domain.account.NewOCOResponse;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public class CancelOrderListResponse extends NewOCOResponse {
-
-}
