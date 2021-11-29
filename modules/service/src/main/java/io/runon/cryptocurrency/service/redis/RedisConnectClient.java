8800ec2253cde06d8008ce218037a6becf6e581c
macle86 <ysys86a@gmail.com>
2021-11-29 09:47:39 +0900

    2021

--- modules/service/src/main/java/io/runon/cryptocurrency/service/redis/RedisConnectClient.java
+++ modules/service/src/main/java/io/runon/cryptocurrency/service/redis/RedisConnectClient.java
@@ -1,6 +1,5 @@
 package io.runon.cryptocurrency.service.redis;
 
-import com.seomse.commons.config.Config;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulConnection;
 import io.lettuce.core.api.StatefulRedisConnection;
@@ -11,7 +10,6 @@ import io.lettuce.core.api.sync.RedisCommands;
 import io.lettuce.core.api.sync.RedisHashCommands;
 import io.lettuce.core.api.sync.RedisStringCommands;
 import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
-import io.runon.cryptocurrency.service.CryptocurrencyYml;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
@@ -25,23 +23,12 @@ public class RedisConnectClient implements RedisConnect{
     private final RedisClient redisClient;
     private StatefulRedisConnection<String, String> connection;
 
-    RedisConnectClient(){
-        String host;
-        String port;
-        try {
-            Map<String, Object> redisMap = CryptocurrencyYml.getYmlMap("redis");
-            host = redisMap.get("host").toString();
-            port = redisMap.get("port").toString();
-        }catch(Exception e){
-            log.debug(e.getMessage());
-            log.debug("redis seomse config use");
-
-            host = Config.getConfig("cryptocurrency.redis.host", "cryptocurrency.runon.io");
-            port = Config.getConfig("cryptocurrency.redis.port", "16379");
-        }
-        redisClient =  RedisClient.create("redis://" + host + ":" +port);
-
+    RedisConnectClient(Map<String, Object> redisMap){
+        redisClient =  RedisClient.create("redis://" + redisMap.get("host") + ":" + redisMap.get("port"));
     }
+
+
+
     private RedisAsyncCommands<String, String> async;
     private RedisCommands<String , String > sync;
     @Override
--- modules/service/src/main/java/io/runon/cryptocurrency/service/redis/RedisConnectCluster.java
+++ modules/service/src/main/java/io/runon/cryptocurrency/service/redis/RedisConnectCluster.java
@@ -5,41 +5,67 @@ import io.lettuce.core.api.async.RedisHashAsyncCommands;
 import io.lettuce.core.api.async.RedisStringAsyncCommands;
 import io.lettuce.core.api.sync.RedisHashCommands;
 import io.lettuce.core.api.sync.RedisStringCommands;
+import io.lettuce.core.cluster.RedisClusterClient;
+import io.lettuce.core.cluster.api.StatefulRedisClusterConnection;
+import io.lettuce.core.cluster.api.async.RedisAdvancedClusterAsyncCommands;
+import io.lettuce.core.cluster.api.sync.RedisAdvancedClusterCommands;
 import io.lettuce.core.pubsub.StatefulRedisPubSubConnection;
 
+import java.util.Map;
+/**
+ * @author macle
+ */
 public class RedisConnectCluster implements RedisConnect{
+
+    private final RedisClusterClient redisClusterClient;
+    private StatefulRedisClusterConnection<String, String> connection;
+    RedisConnectCluster(Map<String, Object> redisMap){
+        redisClusterClient= RedisClusterClient.create("redis://" + redisMap.get("host") + ":" + redisMap.get("port"));
+    }
+
+    private RedisAdvancedClusterAsyncCommands<String, String> async;
+    private RedisAdvancedClusterCommands<String, String> sync;
     @Override
     public RedisStringCommands<String, String> syncString() {
-        return null;
+        return sync;
     }
 
     @Override
     public RedisHashCommands<String, String> syncHash() {
-        return null;
+        return sync;
     }
 
     @Override
     public RedisStringAsyncCommands<String, String> asyncString() {
-        return null;
+        return async;
     }
 
     @Override
     public RedisHashAsyncCommands<String, String> asyncHash() {
-        return null;
+        return async;
     }
 
     @Override
     public StatefulConnection<String, String> connection() {
-        return null;
+        connection = redisClusterClient.connect();
+        connection.setAutoFlushCommands(true);
+        async = connection.async();
+        sync = connection.sync();
+        return connection;
     }
 
     @Override
     public StatefulRedisPubSubConnection<String, String> connectPubSub() {
-        return null;
+        return redisClusterClient.connectPubSub();
     }
 
     @Override
-    public void close() throws Exception {
-
+    public void close() {
+        try{
+            connection.close();
+        }catch(Exception ignore){}
+        try{
+            connection.closeAsync();
+        }catch(Exception ignore){}
     }
 }
