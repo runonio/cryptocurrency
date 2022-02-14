7a81f1ab70db91357efc730f865f1e2e78aad9e2
macle86 <ysys86a@gmail.com>
2022-02-15 00:49:12 +0900

    2022

--- modules/exchanges/src/test/java/example/binance/BinanceCandleExample.java
+++ modules/exchanges/src/test/java/example/binance/BinanceCandleExample.java
@@ -1,5 +1,6 @@
 package example.binance;
 
+import com.seomse.commons.utils.time.Times;
 import com.seomse.commons.utils.time.YmdUtil;
 import io.runon.cryptocurrency.exchanges.binance.BinanceCandle;
 
@@ -10,10 +11,13 @@ import io.runon.cryptocurrency.exchanges.binance.BinanceCandle;
 public class BinanceCandleExample {
 
     public static void main(String[] args) {
-        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_1.csv","BTCUSDT", "1m", YmdUtil.getTime("20220101"), null, 1500);
-        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_3.csv","BTCUSDT", "3m", YmdUtil.getTime("20220101"), null, 1500);
-        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_5.csv","BTCUSDT", "5m", YmdUtil.getTime("20220101"), null, 1500);
-        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_15.csv","BTCUSDT", "15m", YmdUtil.getTime("20220101"), null, 1500);
-        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_30.csv","BTCUSDT", "30m", YmdUtil.getTime("20220101"), null, 1500);
+//        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_1.csv","BTCUSDT", "1m", YmdUtil.getTime("20220101"), null, 1500);
+//        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_3.csv","BTCUSDT", "3m", YmdUtil.getTime("20220101"), null, 1500);
+//        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_5.csv","BTCUSDT", "5m", YmdUtil.getTime("20220101"), null, 1500);
+//        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_15.csv","BTCUSDT", "15m", YmdUtil.getTime("20220101"), null, 1500);
+//        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_30.csv","BTCUSDT", "30m", YmdUtil.getTime("20220101"), null, 1500);
+
+
+        BinanceCandle.csv(BinanceCandle.FUTURES_CANDLE, "candle_1.csv", "BTCUSDT", Times.MINUTE_1, YmdUtil.getTime("20220101"), 1500 );
     }
 }
