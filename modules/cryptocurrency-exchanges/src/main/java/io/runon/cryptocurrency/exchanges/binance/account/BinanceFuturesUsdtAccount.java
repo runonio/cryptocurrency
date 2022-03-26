c07891bee513e3cbbe86bebb8d60ae01a75a6081
macle86 <ysys86a@gmail.com>
2022-03-26 11:59:16 +0900

    2022

--- modules/cryptocurrency-exchanges/src/main/java/io/runon/cryptocurrency/exchanges/binance/account/BinanceFuturesUsdtAccount.java
+++ modules/cryptocurrency-exchanges/src/main/java/io/runon/cryptocurrency/exchanges/binance/account/BinanceFuturesUsdtAccount.java
@@ -8,12 +8,11 @@ import com.binance.client.model.enums.OrderType;
 import com.binance.client.model.market.ExchangeInfoEntry;
 import com.binance.client.model.market.ExchangeInformation;
 import com.binance.client.model.trade.AccountInformation;
-import com.binance.client.model.trade.Asset;
 import com.binance.client.model.trade.Order;
 import com.binance.client.model.trade.Position;
+import io.runon.cryptocurrency.exchanges.binance.BinanceFuturesApis;
 import io.runon.trading.Trade;
 import io.runon.trading.account.FuturesPosition;
-import io.runon.trading.account.FuturesPositionData;
 import io.runon.trading.account.FuturesTradeAccount;
 import io.runon.trading.exception.MinOrderException;
 import io.runon.trading.exception.SymbolNotFoundException;
@@ -21,7 +20,6 @@ import io.runon.trading.order.MarketPriceOrder;
 import io.runon.trading.order.MarketPriceOrderData;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.math.RoundingMode;
 import java.util.List;
 
@@ -47,60 +45,19 @@ public class BinanceFuturesUsdtAccount implements FuturesTradeAccount {
         this.id = id;
     }
 
+    public BinanceFuturesUsdtAccount(SyncRequestClient syncRequestClient){
+        this.syncRequestClient = syncRequestClient;
+        id = "binance_futures";
+    }
+
+    public BinanceFuturesUsdtAccount(String id, SyncRequestClient syncRequestClient){
+        this.syncRequestClient = syncRequestClient;
+        this.id = id;
+    }
+
     @Override
-    public FuturesPositionData getPosition(String symbol) {
-        return getPosition(symbol, syncRequestClient.getAccountInformation());
-    }
-
-    public static FuturesPositionData getPosition(String symbol, AccountInformation accountInformation) {
-
-        symbol = getSymbol(symbol);
-
-        if(!symbol.endsWith("USDT")){
-            symbol = symbol.toUpperCase();
-            if(!symbol.endsWith("USDT")){
-                symbol = symbol + "USDT";
-            }
-        }
-
-        FuturesPositionData futuresPosition = new FuturesPositionData();
-        futuresPosition.setSymbol(symbol);
-
-        Position symbolPosition = getBinancePosition(symbol, accountInformation);
-
-        if(symbolPosition == null){
-            return futuresPosition;
-        }
-
-        futuresPosition.setPrice(new BigDecimal(symbolPosition.getEntryPrice()));
-        futuresPosition.setSize(symbolPosition.getInitialMargin());
-        futuresPosition.setLeverage(symbolPosition.getLeverage());
-
-        io.runon.trading.strategy.Position p ;
-        if(futuresPosition.getPrice().compareTo(BigDecimal.ZERO) == 0){
-            p = io.runon.trading.strategy.Position.NONE;
-        }else{
-            if(symbolPosition.getPositionAmt().compareTo(BigDecimal.ZERO) < 0 ) {
-                p = io.runon.trading.strategy.Position.SHORT;
-            }else{
-                p = io.runon.trading.strategy.Position.LONG;
-            }
-        }
-
-        futuresPosition.setPosition(p);
-        return futuresPosition;
-    }
-
-    public static Position getBinancePosition(String symbol,  AccountInformation accountInformation){
-        List<Position> positions = accountInformation.getPositions();
-        Position symbolPosition = null;
-        for(Position position : positions) {
-            if (position.getSymbol().equals(symbol)) {
-                symbolPosition = position;
-                break;
-            }
-        }
-        return symbolPosition;
+    public FuturesPosition getPosition(String symbol) {
+        return BinanceFuturesApis.getPosition(symbol, syncRequestClient.getAccountInformation());
     }
 
     @Override
@@ -111,47 +68,13 @@ public class BinanceFuturesUsdtAccount implements FuturesTradeAccount {
     @Override
     public BigDecimal getAssets() {
         AccountInformation accountInformation = syncRequestClient.getAccountInformation();
-        return getAssets(accountInformation);
-    }
-
-    public static BigDecimal getAssets(AccountInformation accountInformation){
-        List<Asset> list = accountInformation.getAssets();
-        Asset usdt = null;
-        for(Asset asset : list){
-            if(asset.getAsset().equals("USDT")){
-                usdt = asset;
-                break;
-            }
-        }
-
-        if(usdt == null){
-            return BigDecimal.ZERO;
-        }
-
-        return usdt.getMarginBalance();
+        return BinanceFuturesApis.getUsdtAsset(accountInformation);
     }
 
     @Override
     public BigDecimal getCash() {
         AccountInformation accountInformation = syncRequestClient.getAccountInformation();
-        return getCash(accountInformation);
-    }
-
-    public static BigDecimal getCash(AccountInformation accountInformation){
-        List<Asset> list = accountInformation.getAssets();
-        Asset usdt = null;
-        for(Asset asset : list){
-            if(asset.getAsset().equals("USDT")){
-                usdt = asset;
-                break;
-            }
-        }
-
-        if(usdt == null){
-            return BigDecimal.ZERO;
-        }
-
-        return usdt.getMaxWithdrawAmount();
+        return BinanceFuturesApis.getUsdtCash(accountInformation);
     }
 
     public SyncRequestClient getSyncRequestClient() {
@@ -173,20 +96,10 @@ public class BinanceFuturesUsdtAccount implements FuturesTradeAccount {
         return futuresPosition.getLeverage();
     }
 
-    public static String getSymbol(String symbol){
-        if(!symbol.endsWith("USDT")){
-            symbol = symbol.toUpperCase();
-            if(!symbol.endsWith("USDT")){
-                symbol = symbol + "USDT";
-            }
-        }
-        return symbol;
-    }
-
     @Override
     public MarketPriceOrder marketPriceOrder(String symbol, Trade.Type type, BigDecimal cash) {
 
-        symbol = getSymbol(symbol);
+        symbol = BinanceFuturesApis.getUsdtSymbol(symbol);
 
         ExchangeInformation exchangeInformation = syncRequestClient.getExchangeInformation();
         List<ExchangeInfoEntry> symbols = exchangeInformation.getSymbols();
@@ -223,40 +136,39 @@ public class BinanceFuturesUsdtAccount implements FuturesTradeAccount {
         Order order = syncRequestClient.postOrder(symbol, OrderSide.valueOf(type.toString()), null,  OrderType.MARKET  , null,
                 quantity.toString(), null, null, null, null, null, NewOrderRespType.RESULT);
 
-        BigDecimal tradePrice = BigDecimal.ONE.divide(order.getExecutedQty(), MathContext.DECIMAL128).multiply(order.getCumQuote())
-                .setScale(2,RoundingMode.HALF_UP).stripTrailingZeros();
-
         MarketPriceOrderData marketPriceOrderData = new MarketPriceOrderData();
-        marketPriceOrderData.setTradePrice(tradePrice);
+        marketPriceOrderData.setTradeType(type);
+        marketPriceOrderData.setQuantity(order.getExecutedQty());
+        marketPriceOrderData.setTradePrice(BinanceFuturesApis.getTradePrice(order,2));
         return marketPriceOrderData;
     }
 
     @Override
     public MarketPriceOrder closePosition(String symbol) {
         AccountInformation accountInformation = syncRequestClient.getAccountInformation();
-        Position symbolPosition = getBinancePosition(symbol, accountInformation);
+        Position symbolPosition = BinanceFuturesApis.getBinancePosition(symbol, accountInformation);
 
         BigDecimal quantity = symbolPosition.getPositionAmt();
 
-        OrderSide orderSide;
-        if(quantity.compareTo(new BigDecimal(0)) > 0){
-            orderSide = OrderSide.SELL;
-        } else {
-            quantity = quantity.multiply(new BigDecimal(-1));
-            orderSide = OrderSide.BUY;
+        if(quantity.compareTo(BigDecimal.ZERO) == 0){
+            return MarketPriceOrder.EMPTY_MARKET_ORDER;
         }
 
-        if(quantity.compareTo(BigDecimal.ZERO) == 0){
-            return null;
+        OrderSide orderSide;
+        if(quantity.compareTo(new BigDecimal(0)) > 0){
+            orderSide = OrderSide.BUY;
+        } else {
+            quantity = quantity.multiply(new BigDecimal(-1));
+            orderSide = OrderSide.SELL;
         }
 
         Order order = syncRequestClient.postOrder(symbol, orderSide, null,  OrderType.MARKET , null,
                 quantity.toString(), null, "true", null, null, null, NewOrderRespType.RESULT);
-        BigDecimal tradePrice = BigDecimal.ONE.divide(order.getExecutedQty(), MathContext.DECIMAL128).multiply(order.getCumQuote())
-                .setScale(2,RoundingMode.HALF_UP).stripTrailingZeros();
 
         MarketPriceOrderData marketPriceOrderData = new MarketPriceOrderData();
-        marketPriceOrderData.setTradePrice(tradePrice);
+        marketPriceOrderData.setTradeType(Trade.Type.valueOf(orderSide.toString()));
+        marketPriceOrderData.setQuantity(order.getExecutedQty());
+        marketPriceOrderData.setTradePrice(BinanceFuturesApis.getTradePrice(order,2));
         return marketPriceOrderData;
     }
 }
