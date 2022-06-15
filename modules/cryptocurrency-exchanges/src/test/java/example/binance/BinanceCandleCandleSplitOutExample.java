247446b8fe5b33bf8f656cde7b45f6fad0c9ce1c
macle86 <ysys86a@gmail.com>
2022-06-15 22:00:01 +0900

    2022

---
+++ modules/cryptocurrency-exchanges/src/test/java/example/binance/BinanceCandleCandleSplitOutExample.java
@@ -0,0 +1,19 @@
+package example.binance;
+
+import com.seomse.commons.utils.time.Times;
+import com.seomse.commons.utils.time.YmdUtil;
+import io.runon.cryptocurrency.exchanges.binance.BinanceCandle;
+
+import java.time.ZoneId;
+
+/**
+ * 바이낸스 캔들 출력 예제
+ * 마지막 캔들정보가 오차되는 문제 해결
+ * @author macle
+ */
+public class BinanceCandleCandleSplitOutExample {
+
+    public static void main(String[] args) {
+        BinanceCandle.csvSplit(BinanceCandle.FUTURES_CANDLE, "yyyyMM", ZoneId.of("Asia/Seoul"), "data/candle","BTCUSDT", Times.MINUTE_1, YmdUtil.getTime("20220101"));
+    }
+}
