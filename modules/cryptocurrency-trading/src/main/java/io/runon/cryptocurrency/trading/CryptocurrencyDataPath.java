27abf1b4d82bcf14e14f67c866fc4750f6562b26
macle <ysys86a@gmail.com>
2024-09-02 06:03:27 +0900

    2024

--- modules/cryptocurrency-trading/src/main/java/io/runon/cryptocurrency/trading/CryptocurrencyDataPath.java
+++ modules/cryptocurrency-trading/src/main/java/io/runon/cryptocurrency/trading/CryptocurrencyDataPath.java
@@ -1,6 +1,10 @@
 package io.runon.cryptocurrency.trading;
 
 import com.seomse.commons.config.Config;
+import io.runon.trading.TradingConfig;
+import io.runon.trading.data.TradingDataPath;
+
+import java.nio.file.FileSystems;
 
 /**
  * 암호화폐 데이터 경로
@@ -13,12 +17,15 @@ public class CryptocurrencyDataPath {
      * @return 현물 캔들 데이터 경로
      */
     public static String getSpotCandleDirPath() {
-        String spotCandleDirPath = Config.getConfig("cryptocurrency.spot.candle.dir.path");
-        if (spotCandleDirPath == null) {
-            spotCandleDirPath = Config.getConfig("trading.data.path", "runon/data") + "/cryptocurrency/spot/candle";
+        String dirPath = Config.getConfig("cryptocurrency.spot.candle.dir.path");
+        if (dirPath == null) {
+            String fileSeparator = FileSystems.getDefault().getSeparator();
+            dirPath = TradingConfig.getTradingDataPath() + "/cryptocurrency/spot/candle";
         }
 
-        return spotCandleDirPath;
+        dirPath = TradingDataPath.getAbsolutePath(dirPath);
+
+        return dirPath;
     }
 
     /**
@@ -26,12 +33,14 @@ public class CryptocurrencyDataPath {
      * @return 선물 캔들 데이터 경로
      */
     public static String getFuturesCandleDirPath() {
-        String futuresCandleDirPath = Config.getConfig("cryptocurrency.futures.candle.dir.path");
-        if (futuresCandleDirPath == null) {
-            futuresCandleDirPath = Config.getConfig("trading.data.path", "runon/data") + "/cryptocurrency/futures/candle";
+        String dirPath = Config.getConfig("cryptocurrency.futures.candle.dir.path");
+        if (dirPath == null) {
+            dirPath = TradingConfig.getTradingDataPath() + "/cryptocurrency/futures/candle";
         }
 
-        return futuresCandleDirPath;
+        dirPath = TradingDataPath.getAbsolutePath(dirPath);
+
+        return dirPath;
     }
 
     /**
@@ -41,9 +50,9 @@ public class CryptocurrencyDataPath {
     public static String getSpotOrderBookDirPath() {
         String dirPath = Config.getConfig("cryptocurrency.spot.order.book.dir.path");
         if (dirPath == null) {
-            dirPath = Config.getConfig("trading.data.path", "runon/data") + "/cryptocurrency/spot/order_book";
+            dirPath = TradingConfig.getTradingDataPath() + "/cryptocurrency/spot/order_book";
         }
-
+        dirPath = TradingDataPath.getAbsolutePath(dirPath);
         return dirPath;
     }
 
@@ -54,9 +63,9 @@ public class CryptocurrencyDataPath {
     public static String getFuturesOrderBookDirPath() {
         String dirPath = Config.getConfig("cryptocurrency.futures.order.book.dir.path");
         if (dirPath == null) {
-            dirPath = Config.getConfig("trading.data.path", "runon/data") + "/cryptocurrency/futures/order_book";
+            dirPath = TradingConfig.getTradingDataPath() + "/cryptocurrency/futures/order_book";
         }
-
+        dirPath = TradingDataPath.getAbsolutePath(dirPath);
         return dirPath;
     }
 
@@ -67,9 +76,9 @@ public class CryptocurrencyDataPath {
     public static String getOpenInterestDirPath() {
         String dirPath = Config.getConfig("cryptocurrency.open.interest.dir.path");
         if (dirPath == null) {
-            dirPath = Config.getConfig("trading.data.path", "runon/data") + "/cryptocurrency/futures/open_interest";
+            dirPath = TradingConfig.getTradingDataPath() + "/cryptocurrency/futures/open_interest";
         }
-
+        dirPath = TradingDataPath.getAbsolutePath(dirPath);
         return dirPath;
     }
 
@@ -81,13 +90,15 @@ public class CryptocurrencyDataPath {
     public static String getMergeVolumeDirPath() {
         String dirPath = Config.getConfig("cryptocurrency.merge.volume.dir.path");
         if (dirPath == null) {
-            dirPath = Config.getConfig("trading.data.path", "runon/data") + "/cryptocurrency/merge/volume";
+            dirPath = TradingConfig.getTradingDataPath() + "/cryptocurrency/merge/volume";
         }
-
+        dirPath = TradingDataPath.getAbsolutePath(dirPath);
         return dirPath;
     }
 
     public static void main(String[] args) {
+
+        System.out.println(getFuturesCandleDirPath());
         System.out.println(getOpenInterestDirPath());
     }
 
